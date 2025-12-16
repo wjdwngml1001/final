beff32b7a6e246e55b3b5dc08eebd625a76e7c6e
wjdwngml1001 <wjdwngml1001@khu.ac.kr>
2025-12-16 12:30:18 +0900

    fix

--- PhotoBlogServer/blog/models.py
+++ PhotoBlogServer/blog/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
     title=models.CharField(max_length=200)
     text=models.TextField()
     created_date=models.DateTimeField(default=timezone.now)
-    published_date=models.DateTimeField(blank=True, null=True)
+    published_date=models.DateTimeField(default=timezone.now)
     image=models.ImageField(upload_to='blog_image/%Y/%m/%d/',default='blog_image/default_error.png')
     def publish(self):
         self.published_date=timezone.now()
